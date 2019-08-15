4acacaacbff1ce07b8ce682b60e4122a5765a2f4
杜彭 <dupeng@bbrightway.com>
2019-08-15 09:51:53 +0800

    remove useless code

--- PCA.py
+++ PCA.py
@@ -4,24 +4,6 @@ import numpy.linalg as npl;
 import DataHelper;
 
 
-# # each column is a sample
-# def projectData(dataSet, percent):
-#     count = dataSet.shape[1];
-#     cov = np.matmul(dataSet, dataSet.T) / count;
-#     u, s, v = npl.svd(cov);
-#
-#     p, totalVar = None, sum(s);
-#
-#     for i in range(0, len(s)):
-#         if sum(s[0:i+1]) >= percent * totalVar:
-#             p = u[:, 0:i+1];
-#             break;
-#
-#     result = np.matmul(p.T, dataSet);
-#
-#     return result, p;
-
-
 def projectData(X, percent):
     X, mu, sigma = DataHelper.normalizeFeatures(X);
 
