20b15a4a86b1f9bb930abd051e9566f8a5a6ac76
杜彭 <dupeng@bbrightway.com>
2019-08-19 14:16:37 +0800

    add code to change data range from 0 to 1

--- CutForestTest.py
+++ CutForestTest.py
@@ -8,12 +8,15 @@ import CutForest;
 
 def testCutForest():
     data1 = np.mat(np.load(r"E:\Data\PARS\JNLH\YuanLiaoDaiShui\trainSet\train_all_75.npy"));
-    data2, mu, sigma, data3, p = PCA.projectData(data1, 0.999);
-    trainSet = data2;
+    # data2, mu, sigma, data3, p = PCA.projectData(data1, 0.999);
+    # minValues, maxValues = data1.min(0), data1.max(0);
+    # trainSet = (data1 - minValues) / (maxValues - minValues);
+    trainSet = data1;
 
     testSet = np.mat(np.load(r"E:\Data\PARS\JNLH\YuanLiaoDaiShui\testSet\test_all_75.npy"));
-    testSet = (testSet - mu) / sigma;
+    # testSet = (testSet - mu) / sigma;
     # testSet = PCA.performProject(testSet, mu, sigma, p);
+    # testSet = (testSet - minValues) / (maxValues - minValues);
 
     index = 9;
     forest = CutForest.CutForest(treeCount = 200, subSamplingSize = 2 ** index);
