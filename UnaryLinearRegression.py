84c9076d4c3cffacf3ecd8b1bb66703fc5f8c7b3
xphter <shania_d@hotmail.com>
2020-04-07 17:27:45 +0800

    add interval prediction

--- UnaryLinearRegression.py
+++ UnaryLinearRegression.py
@@ -16,33 +16,38 @@ class UnaryLinearRegression:
         self.__sigma = None;
         self.__rss = None;
         self.__r2 = None;
+        self.__sx2 = None;
         self.__beta0Std = None;
         self.__beta0T = None;
         self.__beta0P = None;
+        self.__beta0Value = None;
         self.__beta1Std = None;
         self.__beta1T = None;
         self.__beta1P = None;
+        self.__beta1Value = None;
 
 
     def __repr__(self):
-        return "Y = {0} {3} {1} * (X - {2})".format(self.__beta0, math.fabs(self.__beta1), self.__avgX, "+" if self.__beta1 >= 0 else "-");
+        return "y = {0} {3} {1} * (x - {2})".format(self.__beta0Value, math.fabs(self.__beta1Value), self.__avgX, "+" if self.__beta1Value >= 0 else "-");
 
 
     def __str__(self):
-        return "Y = beta0 + beta1 * (X - avgX)\r\n{0}\r\n{1}\r\n{2}".format(
-            "beta0 = {0}, std = {1}, t-value = {2}, p-value = {3}".format(self.__beta0, self.__beta0Std, self.__beta0T, self.__beta0P),
-            "beta1 = {0}, std = {1}, t-value = {2}, p-value = {3}".format(self.__beta1, self.__beta1Std, self.__beta1T, self.__beta1P),
-            "sigma = {0}, R^2 = {1}".format(self.__sigma, self.__r2));
+        return "y = β0 + β1 * (x - {0})\r\n{1}\r\n{2}\r\n{3}".format(
+            self.__avgX,
+            "β0 = {0}, std = {1}, t-value = {2}, p-value = {3}".format(self.__beta0, self.__beta0Std, self.__beta0T, self.__beta0P),
+            "β1 = {0}, std = {1}, t-value = {2}, p-value = {3}".format(self.__beta1, self.__beta1Std, self.__beta1T, self.__beta1P),
+            "σ = {0}, R^2 = {1}".format(self.__sigma, self.__r2)
+        );
 
 
     @property
     def intercept(self):
-        return self.__beta0 - self.__beta1 * self.__avgX;
+        return self.__beta0Value - self.__beta1Value * self.__avgX;
 
 
     @property
     def slop(self):
-        return self.__beta1;
+        return self.__beta1Value;
 
 
     @property
@@ -84,35 +89,48 @@ class UnaryLinearRegression:
         self.__sigma = math.sqrt(rss / (n - 2));
         self.__rss = rss;
         self.__r2 = 1 - rss/tss if tss != 0 else 0;
+        self.__sx2 = sx2;
         self.__beta0Std = self.__sigma / math.sqrt(n);
         self.__beta0T = self.__beta0 / self.__beta0Std if self.__beta0Std != 0 else math.nan;
         self.__beta0P = self.__getP(self.__beta0T, n - 2) if self.__beta0Std != 0 else 0;
-        if self.__beta0P > sigLevel:
-            self.__beta0 = 0;
+        self.__beta0Value = self.__beta0 if self.__beta0P < sigLevel else 0;
 
         self.__beta1Std = self.__sigma / math.sqrt(sx2);
         self.__beta1T = self.__beta1 / self.__beta1Std if self.__beta1Std != 0 else math.nan;
         self.__beta1P = self.__getP(self.__beta1T, n - 2) if self.__beta1Std != 0 else 0;
-        if self.__beta1P > sigLevel:
-            self.__beta1 = 0;
+        self.__beta1Value = self.__beta1 if self.__beta1P < sigLevel else 0;
 
 
-    def predict(self, x):
+    def predictValue(self, x):
         if x is None:
             raise ValueError();
 
-        return self.__beta0 + self.__beta1 * (x - self.__avgX);
+        return self.__beta0Value + self.__beta1Value * (x - self.__avgX);
+
+
+    def predictInterval(self, x, confidence = None):
+        if x is None:
+            raise ValueError();
+        if confidence is not None and (confidence <= 0 or confidence >= 1):
+            raise ValueError();
+
+        alpha = 1 - confidence if confidence is not None else UnaryLinearRegression.__DEFAULT_SIG_LEVEL;
+        tValue = t.ppf(1 - alpha / 2, self.__n - 2);
+        interval = np.sqrt(1 + 1 / self.__n + np.power(x - self.__avgX, 2) / self.__sx2) * self.__sigma * tValue;
+        value = self.predictValue(x);
+
+        return np.mat(np.hstack((value - interval, value, value + interval)));
 
 
     def inverse(self, y):
         if y is None:
             raise ValueError();
-        if self.__beta1 == 0:
+        if self.__beta1Value == 0:
             raise NotImplementedError();
 
-        return (y - self.__beta0) / self.__beta1 + self.__avgX;
+        return (y - self.__beta0Value) / self.__beta1Value + self.__avgX;
 
 
     def calcRss(self, x, y):
-        residual = y - self.predict(x);
+        residual = y - self.predictValue(x);
         return (residual.T * residual)[0, 0];
--- UnaryLinearRegressionTest.py
+++ UnaryLinearRegressionTest.py
@@ -13,7 +13,7 @@ def __run(x, y):
 def __show(x, y, line):
     plt.figure(1, (16, 10));
     plt.scatter(x.A.flatten().tolist(), y.A.flatten().tolist(), marker = "x");
-    plt.plot([x.min(), x.max()], [line.predict(x.min()), line.predict(x.max())], color = "r");
+    plt.plot([x.min(), x.max()], [line.predictValue(x.min()), line.predictValue(x.max())], color = "r");
     plt.show();
 
 
@@ -24,6 +24,8 @@ def testUnaryLinearRegression():
 
     x = data[:, 1];
     tv = __run(x, y);
+    tv.predictInterval(x[0, 0]);
+    tv.predictInterval(x);
     __show(x, y, tv);
     print("tv: ");
     print(tv);
@@ -31,6 +33,8 @@ def testUnaryLinearRegression():
 
     x = data[:, 2];
     radio = __run(x, y);
+    radio.predictInterval(x[0, 0]);
+    radio.predictInterval(x);
     __show(x, y, radio);
     print("radio: ");
     print(radio);
@@ -38,6 +42,8 @@ def testUnaryLinearRegression():
 
     x = data[:, 3];
     newspaper = __run(x, y);
+    newspaper.predictInterval(x[0, 0]);
+    newspaper.predictInterval(x);
     __show(x, y, newspaper);
     print("newspaper: ");
     print(newspaper);
