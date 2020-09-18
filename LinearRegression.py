f781355ff2d6a7051ff4108caf17a4bfdd60d5ec
xphter <shania_d@hotmail.com>
2020-09-18 16:52:11 +0800

    use physical cores only

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,6 +1,7 @@
 import abc;
 import math;
 import multiprocessing;
+import psutil;
 import numpy as np;
 from scipy.stats import t, f;
 
@@ -45,7 +46,7 @@ class LinearRegression:
                 data.extend([(X, y, indices) for indices in DataHelper.combinations(p, k)]);
             data = list(map(tuple, np.array(data, np.object)[DataHelper.randomArrangement(len(data)), :].tolist()));
 
-            with multiprocessing.Pool(multiprocessing.cpu_count() - 2) as pool:
+            with multiprocessing.Pool(psutil.cpu_count(False) - 2) as pool:
                 models = pool.starmap(LinearRegression._optimalSubsetsCore, data);
 
             for k in range(1, p + 1):
