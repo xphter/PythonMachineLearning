f681537d1e6641b5cd783bc3d944c78beee3d4ac
xphter <shania_d@hotmail.com>
2021-03-18 13:55:42 +0800

    add class to find optimal value of K

--- KMeans.py
+++ KMeans.py
@@ -1,58 +1,103 @@
+import abc;
+import math;
+import psutil;
+import multiprocessing;
 import numpy as np;
+from matplotlib import pyplot as plt
 
 import DataHelper;
 
 
 class KMeans:
+    @staticmethod
+    def optimalClustering(X, k, count):
+        if X is None:
+            raise ValueError("X is none.");
+        if k < 1:
+            raise ValueError("k is less than one.");
+        if count < 1:
+            raise ValueError("count is less than one.");
+
+        result = None;
+        km = KMeans();
+
+        if count == 1:
+            result = [KMeans._performClustering(km, X, k)];
+        else:
+            with multiprocessing.Pool(max(1, psutil.cpu_count(False) - 2)) as pool:
+                result = pool.starmap(KMeans._performClustering, [(km, X, k) for i in range(count)]);
+
+        return min(result, key = lambda item: item[1].sum());
+
+
+    @staticmethod
+    def optimalK(X, maxK, count, selector):
+        km = KMeans();
+
+        with multiprocessing.Pool(max(1, psutil.cpu_count(False) - 2)) as pool:
+            data = pool.starmap(KMeans._performClustering, [(km, X, k) for i in range(count) for k in range(1, maxK + 1)]);
+
+        data = [min([item for item in data if item[2].shape[0] == k], key = lambda item: item[1].sum()) for k in range(1, maxK + 1)];
+
+        index, distance, center = data[selector.find(km, X, data)];
+        print("the optimal k is {0}, the centers are:\r\n{1}\r\n".format(center.shape[0], center));
+
+        return index, distance, center;
+
+
+    @staticmethod
+    def _performClustering(km, X, k):
+        return km.clustering(X, k);
+
+
     def __init__(self, initialCenterSelector = None):
-        self.__initialCenterSelector = initialCenterSelector if initialCenterSelector is not None else (
-            lambda X, k: self.__randomInitialCenter(X, k));
+        self.__initialCenterSelector = initialCenterSelector if initialCenterSelector is not None else self._randomInitialCenter;
 
 
-    def __randomInitialCenter(self, dataSet, k):
-        index = np.random.randint(0, dataSet.shape[0], 1);
-        center = dataSet[index, :];
+    def _randomInitialCenter(self, X, k):
+        indices = list(range(0, X.shape[0]));
+
+        index = np.random.choice(indices, 1, False);
+        center = X[index, :];
+        indices.remove(index[0]);
 
         if k == 1:
             return center;
 
         for i in range(1, k):
-            dataSet = np.delete(dataSet, index, 0);
-            index = self.__randomFindCenter(dataSet, center);
-            center = np.vstack((center, dataSet[index, :]));
+            index = self._randomFindCenter(X, center, indices);
+            center = np.vstack((center, X[index, :]));
+            indices.remove(index[0]);
+
+        print("the initial {0} centers are:\r\n{1}\r\n".format(k, center));
 
         return center;
 
 
-    def __randomFindCenter(self, dataSet, center):
-        cluster, distance = self.__findCluster(dataSet, center);
-        return DataHelper.choiceProportional(distance);
+    def _randomFindCenter(self, X, center, indices):
+        cluster, distance = self._findCluster(X[indices, :], center);
+        return np.random.choice(indices, 1, False, distance.A.flatten() / distance.sum());
 
 
-    def __findCluster(self, dataSet, center):
-        result = None;
-
-        for i in range(0, center.shape[0]):
-            distance = DataHelper.calcEuclideanDistance(dataSet, center[i, :]);
-
-            result = distance if result is None else np.hstack((result, distance));
+    def _findCluster(self, X, center):
+        result = np.mat([DataHelper.calcEuclideanDistance(X, center[i, :]).A.flatten().tolist() for i in range(0, center.shape[0])]).T;
 
         return result.argmin(1), result.min(1);
 
 
-    def __findCenter(self, dataSet, indices):
-        result = None;
-
-        for i in set(np.sort(indices, None).A.flatten()):
-            center = dataSet[(indices == i).A.flatten(), :].mean(0);
-
-            result = center if result is None else np.vstack((result, center));
-
-        return result;
+    def _findCenter(self, X, indices):
+        return np.mat([X[(indices == i).A.flatten(), :].mean(0).A.flatten().tolist() for i in set(indices.A.flatten().tolist())]);
 
 
-    def clustering(self, dataSet, k, iterationCount = None):
-        if dataSet is None or not isinstance(dataSet, np.matrix):
+    def _calcClusterDiameter(self, X):
+        if X.shape[0] == 0:
+            return 0;
+
+        return DataHelper.calcEuclideanDistance(X, X).max();
+
+
+    def clustering(self, X, k, iterationCount = None):
+        if X is None or not isinstance(X, np.matrix):
             raise ValueError();
 
         if k < 1:
@@ -60,15 +105,103 @@ class KMeans:
 
         count = 0;
         previousIndex, currentIndex, distance = None, None, None;
-        center = self.__initialCenterSelector(dataSet, k);
+        center = self.__initialCenterSelector(X, k);
 
         while (iterationCount is None or iterationCount is not None and count < iterationCount) and \
                 (previousIndex is None or not (currentIndex == previousIndex).all()):
             previousIndex = currentIndex;
-            currentIndex, distance = self.__findCluster(dataSet, center);
+            currentIndex, distance = self._findCluster(X, center);
 
-            center = self.__findCenter(dataSet, currentIndex);
+            center = self._findCenter(X, currentIndex);
 
             count += 1;
 
+        # diameter = np.mat([self._calcClusterDiameter(X[(currentIndex == i).A.flatten(), :]) for i in range(0, center.shape[0])]).T;
+
+        print("the final {0} centers are:\r\n{1}\r\n".format(k, center));
+
         return currentIndex, distance, center;
+
+
+class IOptimalKSelector(metaclass = abc.ABCMeta):
+    @abc.abstractmethod
+    def find(self, km, X, data):
+        pass;
+
+
+class ElbowMethod(IOptimalKSelector):
+    def __init__(self, isShowPlot = False):
+        self.__isShowPlot = bool(isShowPlot);
+
+    def find(self, km, X, data):
+        error = [item[1].sum() for item in data];
+
+        # can't select zero index for ever
+        index = np.diff(error, 2).argmax() + 1;
+
+        if self.__isShowPlot:
+            plt.figure(1, (12, 8));
+            plt.get_current_fig_manager().window.maximize();
+            plt.plot([item[2].shape[0] for item in data], error, "-o");
+            plt.show(block=True);
+
+        return index;
+
+
+class GapStatistic(IOptimalKSelector):
+    def __init__(self, count, isShowPlot = False):
+        self.__count = count;
+        self.__isShowPlot = bool(isShowPlot);
+
+
+    def _calcMeasures(self, km, X, k, i):
+        index, distance, center = km.clustering(X, k);
+        return i, math.log(distance.sum());
+
+
+    def _calcSK(self, wk, mu, B):
+        v = np.mat(wk) - mu;
+
+        return math.sqrt((1 + B) * (v * v.T)[0, 0]) / B;
+
+
+    def find(self, km, X, data):
+        n, p = X.shape;
+        B = self.__count;
+        minValue, maxValue = X.min(0), X.max(0);
+        delta = maxValue - minValue;
+
+        K = [item[2].shape[0] for item in data];
+        WK = [math.log(item[1].sum()) for item in data];
+
+        referenceWK = None;
+        referenceX = [np.multiply(np.mat([np.random.uniform(0, 1, n) for j in range(p)]).T, delta) + minValue for b in range(B)];
+
+        with multiprocessing.Pool(max(1, psutil.cpu_count(False) - 2)) as pool:
+            referenceWK = pool.starmap(self._calcMeasures, [(km, referenceX[b], K[i], i) for b in range(B) for i in range(len(data))]);
+
+        mu = [np.mean([item[1] for item in referenceWK if item[0] == i]) for i in range(len(data))];
+
+        sK = [self._calcSK([item[1] for item in referenceWK if item[0] == i], mu[i], B) for i in range(len(data))];
+        print("the sK is:\r\n{0}\r\n".format(sK));
+
+        gapK = (np.array(mu) - np.array(WK)).tolist();
+        print("the Gap K is:\r\n{0}\r\n".format(gapK));
+
+        # index = 0;
+        # for i in range(len(data) - 1):
+        #     if gapK[i] >= gapK[i + 1] - sK[i + 1]:
+        #         index = i;
+        #         break;
+        index = np.argmax(gapK);
+
+        if self.__isShowPlot:
+            plt.figure(1, (12, 8));
+            plt.get_current_fig_manager().window.maximize();
+            plt.plot(K, WK, "-ob", label = "logWk");
+            plt.plot(K, mu, "-ok", label = "(Σ logWk) / B");
+            plt.plot(K, gapK, "-or", label = "Gap");
+            plt.legend(loc='upper right');
+            plt.show(block=True);
+
+        return index;
--- KMeansTest.py
+++ KMeansTest.py
@@ -7,18 +7,65 @@ import DataHelper;
 
 
 def testKMeans():
-    data = np.mat(np.loadtxt(r"data\kmeans2.txt", delimiter = "\t"));
-    index, distance, center = KMeans.KMeans().clustering(data, 3);
-    print(center);
+    data = np.mat(np.loadtxt("data/kmeans1.txt", delimiter = "\t"));
+    index, distance, center = KMeans.KMeans.optimalClustering(data, 3, 20000);
+    print("the final center is:\r\n{0}".format(center));
 
-    fig = plt.figure();
-    ax = plt.subplot(111);
+    plt.figure(1, (12, 8));
+    plt.get_current_fig_manager().window.maximize();
 
-    colors = ["g", "b", "y", "k"];
-    markers = ["*", "+", "D", "s"];
+    colors = ["g", "b", "y", "k", "r", "m", "c"];
+    markers = ["*", "+", "D", "s", "h", "v", "d"];
 
     for i in range(0, center.shape[0]):
-        ax.scatter(data[(index == i).A.flatten(), 0].A.flatten(), data[(index == i).A.flatten(), 1].A.flatten(), color = colors[i], marker = markers[i]);
-        ax.scatter(center[i, 0], center[i, 1], color = ["r"], marker = "x");
+        plt.scatter(data[(index == i).A.flatten(), 0].A.flatten(), data[(index == i).A.flatten(), 1].A.flatten(), color = colors[i], marker = markers[i]);
+        plt.scatter(center[i, 0], center[i, 1], color = ["r"], marker = "x");
 
-    plt.show();
+    plt.show(block=True);
+
+
+def ElbowMethod():
+    count = 1;
+    # X = np.mat(np.loadtxt("data/kmeans1.txt", delimiter = "\t"));
+    X = np.mat(np.load("/media/WindowsE/Data/PARS/JNLH/YuanLiaoDaiShui/trainSet/train_all_75.npy"))[:, 57];
+
+    plt.figure(1, (12, 8));
+    plt.get_current_fig_manager().window.maximize();
+    plt.hist(X, bins = 1000);
+    plt.show(block=True);
+
+    selector = KMeans.ElbowMethod(True);
+    index, distance, center = KMeans.KMeans.optimalK(X, 10, count, selector);
+
+    plt.figure(1, (12, 8));
+    plt.get_current_fig_manager().window.maximize();
+    plt.hist(X, bins=1000);
+    for i in range(center.shape[0]):
+        plt.axvline(center[i, 0], color = "r");
+    plt.show(block=True);
+
+    print("the optimal k is {0}".format(center.shape[0]));
+    print("the centers is:\r\n{0}".format(center));
+
+
+def GapStatistic():
+    count = 1;
+    # X = np.mat(np.loadtxt("data/kmeans1.txt", delimiter = "\t"));
+    # X = np.mat(np.load("/media/WindowsE/Data/PARS/JNLH/YuanLiaoDaiShui/trainSet/train_all_75.npy"))[:, 68];#57
+    # X = np.mat(np.load("/media/WindowsD/WorkSpace/data.npy"))[:, 0];
+    X = np.mat(np.load("/media/WindowsE/Data/PARS/JNLH/ReasonAnalysis/60/data.npy"))[:, 1];
+
+    plt.figure(1, (12, 8));
+    plt.get_current_fig_manager().window.maximize();
+    plt.hist(X, bins = 1000);
+    plt.show(block=True);
+
+    selector = KMeans.GapStatistic(20, True);
+    index, distance, center = KMeans.KMeans.optimalK(X, 10, count, selector);
+
+    plt.figure(1, (12, 8));
+    plt.get_current_fig_manager().window.maximize();
+    plt.hist(X, bins=1000);
+    for i in range(center.shape[0]):
+        plt.axvline(center[i, 0], color = "r");
+    plt.show(block=True);
